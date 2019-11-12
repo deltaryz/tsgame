c0d12401e7cdadd4f8ac8af0c8bd7a2318c89af3
techniponi <wincam97@gmail.com>
2019-11-12 17:54:48 -0600

    Reformat code

--- src/entity.ts
+++ src/entity.ts
@@ -11,10 +11,10 @@ import * as room from "./room"; // everything room related!
 
 // this contains metadata of what an entity is/does
 export interface EntityData {
-  name: string;
-  onClick(entity: Entity): void; // what happens when we click the entity with our bare Hands x 1?
-  metaType: ENTITY_META_TYPE;
-  tick?(entity: Entity): void; // what should this entity do when the game ticks?
+    name: string;
+    onClick(entity: Entity): void; // what happens when we click the entity with our bare Hands x 1?
+    metaType: ENTITY_META_TYPE;
+    tick?(entity: Entity): void; // what should this entity do when the game ticks?
 }
 
 // Entity registry
@@ -24,268 +24,268 @@ export let entityRegistry = new Map<ENTITY_TYPE, EntityData>();
 
 // types of entities!
 export enum ENTITY_TYPE {
-  PLAYER = "PLAYER", // this one is special
-  ITEM_ENTITY = "ITEM_ENTITY",
-  PLANT_LIFEBUD = "PLANT_LIFEBUD",
-  OBJECT_STONE = "OBJECT_STONE"
+    PLAYER = "PLAYER", // this one is special
+    ITEM_ENTITY = "ITEM_ENTITY",
+    PLANT_LIFEBUD = "PLANT_LIFEBUD",
+    OBJECT_STONE = "OBJECT_STONE"
 }
 
 // these are used for more generic identification
 export enum ENTITY_META_TYPE {
-  DEFAULT = "DEFAULT", // nothing specific
-  PLANT = "PLANT",
-  OBJECT = "OBJECT"
+    DEFAULT = "DEFAULT", // nothing specific
+    PLANT = "PLANT",
+    OBJECT = "OBJECT"
 }
 
 // Here we define entity metadata and behavior
 entityRegistry
-  .set(ENTITY_TYPE.PLAYER, {
-    name: "Player",
-    onClick: (entity: Entity) => {
-      render.displayToastNotification(
-        "how did you even activate this??? (player was clicked)"
-      ); // this should theoretically never get called
-    },
-    metaType: ENTITY_META_TYPE.DEFAULT
-  })
-  .set(ENTITY_TYPE.ITEM_ENTITY, {
-    name: "Item",
-    onClick: (entity: Entity) => {
-      // TODO: pick up items
-    },
-    metaType: ENTITY_META_TYPE.DEFAULT
-  })
-  .set(ENTITY_TYPE.PLANT_LIFEBUD, {
-    name: "Lifebud",
-    onClick: (entity: Entity) => {
-      if (entity.getGrowth() == 100) {
-        render.displayToastNotification(
-          "You harvested the Lifebud and obtained seeds."
-        );
+    .set(ENTITY_TYPE.PLAYER, {
+        name: "Player",
+        onClick: (entity: Entity) => {
+            render.displayToastNotification(
+                "how did you even activate this??? (player was clicked)"
+            ); // this should theoretically never get called
+        },
+        metaType: ENTITY_META_TYPE.DEFAULT
+    })
+    .set(ENTITY_TYPE.ITEM_ENTITY, {
+        name: "Item",
+        onClick: (entity: Entity) => {
+            // TODO: pick up items
+        },
+        metaType: ENTITY_META_TYPE.DEFAULT
+    })
+    .set(ENTITY_TYPE.PLANT_LIFEBUD, {
+        name: "Lifebud",
+        onClick: (entity: Entity) => {
+            if (entity.getGrowth() == 100) {
+                render.displayToastNotification(
+                    "You harvested the Lifebud and obtained seeds."
+                );
 
-        game.currentGame.getCurrentRoom().removeEntity(entity);
-        game.currentGame
-          .getCurrentPlayer()
-          .getInventory()
-          .createItem("Lifeseed", 3, item.ITEM_TYPE.LIFESEED);
-      } else {
-        render.displayToastNotification(
-          "This Lifebud is not ready to harvest yet. (" +
-            entity.getGrowth() +
-            "%)"
-        );
-      }
-    },
-    metaType: ENTITY_META_TYPE.PLANT,
-    tick: (entity: Entity) => {
-      // this plant will grow a little every tick!
-      entity.grow(25);
+                game.currentGame.getCurrentRoom().removeEntity(entity);
+                game.currentGame
+                    .getCurrentPlayer()
+                    .getInventory()
+                    .createItem("Lifeseed", 3, item.ITEM_TYPE.LIFESEED);
+            } else {
+                render.displayToastNotification(
+                    "This Lifebud is not ready to harvest yet. (" +
+                    entity.getGrowth() +
+                    "%)"
+                );
+            }
+        },
+        metaType: ENTITY_META_TYPE.PLANT,
+        tick: (entity: Entity) => {
+            // this plant will grow a little every tick!
+            entity.grow(25);
 
-      if (entity.getGrowth() >= 100) {
-        // the plant is fully grown!
-        render.displayToastNotification(
-          "A " + entity.getDisplayName() + " has fully grown!"
-        );
-      }
-    }
-  })
-  .set(ENTITY_TYPE.OBJECT_STONE, {
-    name: "Stone",
-    onClick: (entity: Entity) => {
-      render.displayToastNotification("The rock is too heavy to move.");
-    },
-    metaType: ENTITY_META_TYPE.OBJECT
-  });
+            if (entity.getGrowth() >= 100) {
+                // the plant is fully grown!
+                render.displayToastNotification(
+                    "A " + entity.getDisplayName() + " has fully grown!"
+                );
+            }
+        }
+    })
+    .set(ENTITY_TYPE.OBJECT_STONE, {
+        name: "Stone",
+        onClick: (entity: Entity) => {
+            render.displayToastNotification("The rock is too heavy to move.");
+        },
+        metaType: ENTITY_META_TYPE.OBJECT
+    });
 
 // Entity class
 // Any object in the world that is not a tile
 export class Entity {
-  private inventory: item.Inventory; // all entities can have inventories
-  private position: game.Position;
-  private type: ENTITY_TYPE;
-  private displayName: string;
-  private growth: number; // only relevant for plants, defines growth percentage out of 100
+    private inventory: item.Inventory; // all entities can have inventories
+    private position: game.Position;
+    private type: ENTITY_TYPE;
+    private displayName: string;
+    private growth: number; // only relevant for plants, defines growth percentage out of 100
 
-  constructor(
-    type: ENTITY_TYPE,
-    position?: game.Position,
-    inventoryItems?: item.Item[],
-    name?: string,
-    growth?: number
-  ) {
-    let outputPosX: number = 0; // assume 0 if nothign else is given
-    let outputPosY: number = 0;
-    if (position != undefined) {
-      outputPosX = position.x;
-      outputPosY = position.y;
+    constructor(
+        type: ENTITY_TYPE,
+        position?: game.Position,
+        inventoryItems?: item.Item[],
+        name?: string,
+        growth?: number
+    ) {
+        let outputPosX: number = 0; // assume 0 if nothign else is given
+        let outputPosY: number = 0;
+        if (position != undefined) {
+            outputPosX = position.x;
+            outputPosY = position.y;
+        }
+        console.log(
+            "Generating new entity!\nPosition: " +
+            outputPosX +
+            "," +
+            outputPosY +
+            "\ninventoryItems: " +
+            inventoryItems +
+            "\nType: " +
+            type
+        );
+
+        if (position != undefined) {
+            this.position = position;
+        } else {
+            this.position = { x: 0, y: 0 };
+        }
+
+        this.type = type;
+
+        if (name != undefined) {
+            this.displayName = name;
+        } else {
+            this.displayName = entityRegistry.get(type).name;
+        }
+
+        this.inventory = new item.Inventory(inventoryItems);
+
+        if (growth != undefined) {
+            this.growth = growth;
+        } else {
+            if (entityRegistry.get(type).metaType == ENTITY_META_TYPE.PLANT) {
+                // this is some sort of plant, set growth to 0
+                this.growth = 0;
+            } else {
+                // we use -1 to represent something with no growth value
+                this.growth = -1;
+            }
+        }
     }
-    console.log(
-      "Generating new entity!\nPosition: " +
-        outputPosX +
-        "," +
-        outputPosY +
-        "\ninventoryItems: " +
-        inventoryItems +
-        "\nType: " +
-        type
-    );
 
-    if (position != undefined) {
-      this.position = position;
-    } else {
-      this.position = { x: 0, y: 0 };
+    // get position of the entity
+    getPosition(): game.Position {
+        return this.position;
     }
 
-    this.type = type;
-
-    if (name != undefined) {
-      this.displayName = name;
-    } else {
-      this.displayName = entityRegistry.get(type).name;
+    // sets position of an entity
+    setPosition(pos: game.Position) {
+        this.position = pos;
     }
 
-    this.inventory = new item.Inventory(inventoryItems);
-
-    if (growth != undefined) {
-      this.growth = growth;
-    } else {
-      if (entityRegistry.get(type).metaType == ENTITY_META_TYPE.PLANT) {
-        // this is some sort of plant, set growth to 0
-        this.growth = 0;
-      } else {
-        // we use -1 to represent something with no growth value
-        this.growth = -1;
-      }
+    // get the selected texture of an entity
+    getType(): ENTITY_TYPE {
+        return this.type;
     }
-  }
 
-  // get position of the entity
-  getPosition(): game.Position {
-    return this.position;
-  }
+    // set the visible texture of an entity
+    setType(texture: ENTITY_TYPE) {
+        this.type = texture;
+    }
 
-  // sets position of an entity
-  setPosition(pos: game.Position) {
-    this.position = pos;
-  }
+    // returns the entity's inventory
+    getInventory(): item.Inventory {
+        return this.inventory;
+    }
 
-  // get the selected texture of an entity
-  getType(): ENTITY_TYPE {
-    return this.type;
-  }
+    // completely wipes a user's inventory
+    setInventory(inv: item.Inventory) {
+        this.inventory = inv;
+    }
 
-  // set the visible texture of an entity
-  setType(texture: ENTITY_TYPE) {
-    this.type = texture;
-  }
+    // returns display name
+    getDisplayName(): string {
+        return this.displayName;
+    }
 
-  // returns the entity's inventory
-  getInventory(): item.Inventory {
-    return this.inventory;
-  }
+    // sets display name
+    setDisplayName(name: string) {
+        this.displayName = name;
+    }
 
-  // completely wipes a user's inventory
-  setInventory(inv: item.Inventory) {
-    this.inventory = inv;
-  }
+    // get growth value
+    getGrowth(): number {
+        return this.growth;
+    }
 
-  // returns display name
-  getDisplayName(): string {
-    return this.displayName;
-  }
+    // set growth value
+    setGrowth(growth: number) {
+        this.growth = growth;
+    }
 
-  // sets display name
-  setDisplayName(name: string) {
-    this.displayName = name;
-  }
+    // grow by provided value
+    grow(amount: number) {
+        this.growth += amount;
+        if (this.growth >= 100) this.growth = 100;
+    }
 
-  // get growth value
-  getGrowth(): number {
-    return this.growth;
-  }
+    // returns the tick function for this entity
+    getTick() {
+        return entityRegistry.get(this.type).tick;
+    }
 
-  // set growth value
-  setGrowth(growth: number) {
-    this.growth = growth;
-  }
-
-  // grow by provided value
-  grow(amount: number) {
-    this.growth += amount;
-    if (this.growth >= 100) this.growth = 100;
-  }
-
-  // returns the tick function for this entity
-  getTick() {
-    return entityRegistry.get(this.type).tick;
-  }
-
-  // what should we do when this entity is clicked?
-  public onClick = () => {
-    console.log("An entity was clicked!");
-    entityRegistry.get(this.getType()).onClick(this);
-    render.render();
-  };
+    // what should we do when this entity is clicked?
+    public onClick = () => {
+        console.log("An entity was clicked!");
+        entityRegistry.get(this.getType()).onClick(this);
+        render.render();
+    };
 }
 
 // Player class
 // even though this is an Entity, it isn't actually rendered in the world! Position is irrelevant.
 export class Player extends Entity {
-  private name: string;
-  private selectedItem: item.Item;
+    private name: string;
+    private selectedItem: item.Item;
 
-  constructor(
-    name: string,
-    position?: game.Position,
-    inventoryItems?: item.Item[]
-  ) {
-    super(ENTITY_TYPE.PLAYER, position, inventoryItems);
-    console.log("This was the player!\nName: " + name);
-    this.name = name;
-
-    // initialize inventory
-    let defaultHands = new item.Item("Hands", 1, item.ITEM_TYPE.HANDS);
-    this.getInventory().insertItem(defaultHands);
-    this.selectedItem = defaultHands; // this represents nothing
-  }
-
-  // returns the selected tile type
-  getSelectedItem(): item.Item {
-    // go ahead and do a check to make sure that we actually have this item!
-    if (
-      this.getInventory().getItem(this.selectedItem.getItemType()) == undefined
+    constructor(
+        name: string,
+        position?: game.Position,
+        inventoryItems?: item.Item[]
     ) {
-      console.log("We don't have the item that is currently selected!");
-      // we don't!
-      this.setSelectedItem(item.ITEM_TYPE.HANDS);
+        super(ENTITY_TYPE.PLAYER, position, inventoryItems);
+        console.log("This was the player!\nName: " + name);
+        this.name = name;
+
+        // initialize inventory
+        let defaultHands = new item.Item("Hands", 1, item.ITEM_TYPE.HANDS);
+        this.getInventory().insertItem(defaultHands);
+        this.selectedItem = defaultHands; // this represents nothing
     }
-    return this.selectedItem;
-  }
 
-  // sets the selected item type
-  // returns false if the player doesn't have one!
-  setSelectedItem(itemType: item.ITEM_TYPE): boolean {
-    // check if we have the item
-    let item = this.getInventory().getItem(itemType);
-    if (item != undefined) {
-      // player does have this item!
-      console.log("Player's selected item set to " + item.getDisplayName());
-      this.selectedItem = item;
-      render.render(); // make sure the user sees it!
-      return true;
-    } else {
-      // we do not have this item
-      return false;
+    // returns the selected tile type
+    getSelectedItem(): item.Item {
+        // go ahead and do a check to make sure that we actually have this item!
+        if (
+            this.getInventory().getItem(this.selectedItem.getItemType()) == undefined
+        ) {
+            console.log("We don't have the item that is currently selected!");
+            // we don't!
+            this.setSelectedItem(item.ITEM_TYPE.HANDS);
+        }
+        return this.selectedItem;
     }
-  }
 
-  // returns the player's name
-  getName(): string {
-    return this.name;
-  }
+    // sets the selected item type
+    // returns false if the player doesn't have one!
+    setSelectedItem(itemType: item.ITEM_TYPE): boolean {
+        // check if we have the item
+        let item = this.getInventory().getItem(itemType);
+        if (item != undefined) {
+            // player does have this item!
+            console.log("Player's selected item set to " + item.getDisplayName());
+            this.selectedItem = item;
+            render.render(); // make sure the user sees it!
+            return true;
+        } else {
+            // we do not have this item
+            return false;
+        }
+    }
 
-  // sets the player's name
-  setName(name: string) {
-    this.name = name;
-  }
+    // returns the player's name
+    getName(): string {
+        return this.name;
+    }
+
+    // sets the player's name
+    setName(name: string) {
+        this.name = name;
+    }
 }
--- src/tile.ts
+++ src/tile.ts
@@ -9,7 +9,7 @@ import * as room from "./room"; // everything room related!
 
 // this contains the metadata of what the tile is/does
 export interface TileData {
-  name: string;
+    name: string;
 }
 
 // Tile registry
@@ -19,51 +19,51 @@ export let tileRegistry = new Map<TILE_TYPE, TileData>();
 
 // represents which types of tile can exist
 export enum TILE_TYPE {
-  DIRT = "DIRT",
-  STONE = "STONE",
-  WATER = "WATER"
+    DIRT = "DIRT",
+    STONE = "STONE",
+    WATER = "WATER"
 }
 
 // here we define tile metadata and behavior
 tileRegistry
-  .set(TILE_TYPE.DIRT, {
-    name: "Dirt"
-  })
-  .set(TILE_TYPE.STONE, {
-    name: "Stone"
-  })
-  .set(TILE_TYPE.WATER, {
-    name: "Water"
-  });
+    .set(TILE_TYPE.DIRT, {
+        name: "Dirt"
+    })
+    .set(TILE_TYPE.STONE, {
+        name: "Stone"
+    })
+    .set(TILE_TYPE.WATER, {
+        name: "Water"
+    });
 
 // tiles occupy single coordinates of world space
 export class Tile {
-  private type: TILE_TYPE;
-  private position: game.Position;
+    private type: TILE_TYPE;
+    private position: game.Position;
 
-  constructor(type: TILE_TYPE, position: game.Position) {
-    console.log("Generating new tile of type " + type);
-    this.type = type;
-    this.position = position;
-  }
+    constructor(type: TILE_TYPE, position: game.Position) {
+        console.log("Generating new tile of type " + type);
+        this.type = type;
+        this.position = position;
+    }
 
-  // returns the type of tile this is
-  getType(): TILE_TYPE {
-    return this.type;
-  }
+    // returns the type of tile this is
+    getType(): TILE_TYPE {
+        return this.type;
+    }
 
-  // changes the type of the tile
-  setType(type: TILE_TYPE) {
-    this.type = type;
-  }
+    // changes the type of the tile
+    setType(type: TILE_TYPE) {
+        this.type = type;
+    }
 
-  // returns tile's position (this is read only!)
-  getPosition(): game.Position {
-    return this.position;
-  }
+    // returns tile's position (this is read only!)
+    getPosition(): game.Position {
+        return this.position;
+    }
 
-  // returns the name of a tile
-  getName(): string {
-    return tileRegistry.get(this.getType()).name;
-  }
+    // returns the name of a tile
+    getName(): string {
+        return tileRegistry.get(this.getType()).name;
+    }
 }
