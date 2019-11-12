8613775e5fd26add5fb2f179226ef8419b43c3bb
techniponi <wincam97@gmail.com>
2019-11-12 17:51:36 -0600

    Fix switch

--- src/game.ts
+++ src/game.ts
@@ -16,98 +16,98 @@ let tickInterval = 2000; // interval (in milliseconds) of 1 game tick
 
 // what it says on the tin
 function getRandomIntInclusive(min: number, max: number) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive
 }
 
 // keeps track of game state
 class Game {
-  private currentRoom: room.Room;
-  private currentState: GAME_STATE;
-  private currentPlayer: entity.Player;
-  private tickInterval: number;
+    private currentRoom: room.Room;
+    private currentState: GAME_STATE;
+    private currentPlayer: entity.Player;
+    private tickInterval: number;
 
-  // we initialize each of these components separately from the gamestate initialization
-  // this makes it easier to swap them on the fly!
-  constructor(
-    currentRoom: room.Room,
-    currentState: GAME_STATE,
-    currentPlayer: entity.Player,
-    tickInterval: number
-  ) {
-    console.log("Game being initialized!");
-    this.currentRoom = currentRoom;
-    this.currentState = currentState;
-    this.currentPlayer = currentPlayer;
-    this.tickInterval = tickInterval;
-  }
+    // we initialize each of these components separately from the gamestate initialization
+    // this makes it easier to swap them on the fly!
+    constructor(
+        currentRoom: room.Room,
+        currentState: GAME_STATE,
+        currentPlayer: entity.Player,
+        tickInterval: number
+    ) {
+        console.log("Game being initialized!");
+        this.currentRoom = currentRoom;
+        this.currentState = currentState;
+        this.currentPlayer = currentPlayer;
+        this.tickInterval = tickInterval;
+    }
 
-  // this is triggered every time the game ticks
-  tick() {
-    console.log("Tick has passed");
+    // this is triggered every time the game ticks
+    tick() {
+        console.log("Tick has passed");
 
-    // trigger the tick functions of all existing entities
-    this.currentRoom.getEntities().forEach((thisEntity: entity.Entity) => {
-      let tickFunction = thisEntity.getTick();
-      if (tickFunction != undefined) tickFunction(thisEntity);
-    });
-    render.render();
-  }
+        // trigger the tick functions of all existing entities
+        this.currentRoom.getEntities().forEach((thisEntity: entity.Entity) => {
+            let tickFunction = thisEntity.getTick();
+            if (tickFunction != undefined) tickFunction(thisEntity);
+        });
+        render.render();
+    }
 
-  // initializes tick and sets game state
-  startGame() {
-    this.currentState = GAME_STATE.GAMEPLAY;
-    setInterval(() => {
-      this.tick();
-    }, this.tickInterval);
-  }
+    // initializes tick and sets game state
+    startGame() {
+        this.currentState = GAME_STATE.GAMEPLAY;
+        setInterval(() => {
+            this.tick();
+        }, this.tickInterval);
+    }
 
-  // returns the current room
-  getCurrentRoom(): room.Room {
-    return this.currentRoom;
-  }
+    // returns the current room
+    getCurrentRoom(): room.Room {
+        return this.currentRoom;
+    }
 
-  // sets the current room
-  setCurrentRoom(newRoom: room.Room) {
-    console.log("Changing rooms...");
-    this.currentRoom = newRoom;
-  }
+    // sets the current room
+    setCurrentRoom(newRoom: room.Room) {
+        console.log("Changing rooms...");
+        this.currentRoom = newRoom;
+    }
 
-  // returns the current GameState
-  getCurrentState(): GAME_STATE {
-    return this.currentState;
-  }
+    // returns the current GameState
+    getCurrentState(): GAME_STATE {
+        return this.currentState;
+    }
 
-  // sets the current GameState
-  setCurrentState(newState: GAME_STATE) {
-    console.log("Setting game state to to " + newState);
-    this.currentState = newState;
-  }
+    // sets the current GameState
+    setCurrentState(newState: GAME_STATE) {
+        console.log("Setting game state to to " + newState);
+        this.currentState = newState;
+    }
 
-  // returns the current Player
-  getCurrentPlayer(): entity.Player {
-    return this.currentPlayer;
-  }
+    // returns the current Player
+    getCurrentPlayer(): entity.Player {
+        return this.currentPlayer;
+    }
 
-  // sets the current Player
-  setCurrentPlayer(player: entity.Player) {
-    console.log("Setting player to " + player.getName());
-    this.currentPlayer = player;
-  }
+    // sets the current Player
+    setCurrentPlayer(player: entity.Player) {
+        console.log("Setting player to " + player.getName());
+        this.currentPlayer = player;
+    }
 }
 
 // represents what state the game is currently in
 enum GAME_STATE {
-  INITIALIZE = "INITIALIZE",
-  MENU = "MENU",
-  GAMEPLAY = "GAMEPLAY"
+    INITIALIZE = "INITIALIZE",
+    MENU = "MENU",
+    GAMEPLAY = "GAMEPLAY"
 }
 
 // location on map
 export interface Position {
-  x: number;
-  y: number;
+    x: number;
+    y: number;
 }
 
 // INITIALIZE GAME
@@ -128,22 +128,22 @@ entityPlacementQueue.push(defaultStone);
 
 let occupiedPositions: Position[] = []; // keep track of which positions have been used
 entityPlacementQueue.forEach((entity: entity.Entity) => {
-  let currentPosition = getRandomPositionInRoom(defaultRoom);
-  while (occupiedPositions.includes(currentPosition)) {
-    // loop this until position is unique
-    currentPosition = getRandomPositionInRoom(defaultRoom);
-  }
-  occupiedPositions.push(currentPosition);
-  entity.setPosition(currentPosition);
-  defaultRoom.addEntity(entity);
+    let currentPosition = getRandomPositionInRoom(defaultRoom);
+    while (occupiedPositions.includes(currentPosition)) {
+        // loop this until position is unique
+        currentPosition = getRandomPositionInRoom(defaultRoom);
+    }
+    occupiedPositions.push(currentPosition);
+    entity.setPosition(currentPosition);
+    defaultRoom.addEntity(entity);
 });
 
 // this will contain everything relevant to the current game
 currentGame = new Game(
-  defaultRoom,
-  GAME_STATE.INITIALIZE,
-  defaultPlayer,
-  tickInterval
+    defaultRoom,
+    GAME_STATE.INITIALIZE,
+    defaultPlayer,
+    tickInterval
 );
 
 // we keep the rendering code separate so that it can easily be changed or reworked
@@ -151,17 +151,17 @@ currentGame = new Game(
 setTimeout(render.render, 2000);
 
 function getRandomPositionInRoom(room: room.Room): Position {
-  let xPos = getRandomIntInclusive(0, room.getRoomSize()[0] - 1);
-  let yPos = getRandomIntInclusive(0, room.getRoomSize()[1] - 1);
+    let xPos = getRandomIntInclusive(0, room.getRoomSize()[0] - 1);
+    let yPos = getRandomIntInclusive(0, room.getRoomSize()[1] - 1);
 
-  console.log("Random position generated: " + xPos + " " + yPos);
-  return { x: xPos, y: yPos };
+    console.log("Random position generated: " + xPos + " " + yPos);
+    return { x: xPos, y: yPos };
 }
 
 // make sure we wait for pixi to load its assets
 let waitForPixi = setInterval(() => {
-  if (render.ready == true) {
-    clearInterval(waitForPixi);
-    currentGame.startGame();
-  }
+    if (render.ready == true) {
+        clearInterval(waitForPixi);
+        currentGame.startGame();
+    }
 }, 1000);
--- src/render.ts
+++ src/render.ts
@@ -7,6 +7,7 @@ import { currentGame } from "./game"; // Represents the overall game state
 import * as entity from "./entity"; // Needed for displaying entities
 import * as item from "./item"; // Needed for displaying inventory items
 import * as room from "./room"; // everything room related!
+import { TILE_TYPE } from "./tile";
 export let ready = false; // make sure we don't render anything before that has been initialized
 
 let heldItemDisplay = document.getElementById("selectedItemDisplay");
@@ -15,215 +16,215 @@ let inventoryButtonsDiv = document.getElementById("inventoryButtons");
 inventoryButtonsDiv.innerHTML = "Please wait while the game loads!";
 
 export const app = new PIXI.Application({
-  width: 256,
-  height: 256
+    width: 256,
+    height: 256
 });
 
 // load assets here
 // all of them do have to be loaded manually.
 // there should be a spritesheet for this eventually
 app.loader
-  .add("assets/dirt.png")
-  .add("assets/stone.png")
-  .add("assets/water.png")
-  .add("assets/player.png")
-  .add("assets/lifebud.png")
-  .add("assets/lifeseed.png")
-  .load(setup);
+    .add("assets/dirt.png")
+    .add("assets/stone.png")
+    .add("assets/water.png")
+    .add("assets/player.png")
+    .add("assets/lifebud.png")
+    .add("assets/lifeseed.png")
+    .load(setup);
 
 function setup() {
-  document.body.appendChild(app.view);
+    document.body.appendChild(app.view);
 
-  ready = true;
+    ready = true;
 }
 
 // this should be called every time the game updates
 export function render() {
-  if (ready) {
-    //console.log("Updating screen!");
-    while (app.stage.children[0]) {
-      app.stage.removeChild(app.stage.children[0]);
-    } // remove all existing objects
+    if (ready) {
+        //console.log("Updating screen!");
+        while (app.stage.children[0]) {
+            app.stage.removeChild(app.stage.children[0]);
+        } // remove all existing objects
 
-    // UPDATE UI
+        // UPDATE UI
 
-    // show buttons for each inventory item
-    inventoryButtonsDiv.innerHTML = ""; // wipe this clean!
-    currentGame
-      .getCurrentPlayer()
-      .getInventory()
-      .getContents()
-      .forEach(function(item: item.Item) {
-        //sconsole.log("Creating button for item " + item.getDisplayName());
-        let itemButton = document.createElement("BUTTON");
-        if (item.isKeyItem() == true) {
-          // key items won't need to display a quantity
-          itemButton.innerHTML = item.getDisplayName();
-        } else {
-          itemButton.innerHTML =
-            item.getDisplayName() + " x " + item.getQuantity();
-        }
-
-        itemButton.onclick = function() {
-          currentGame.getCurrentPlayer().setSelectedItem(item.getItemType());
-        };
-
-        inventoryButtonsDiv.appendChild(itemButton);
-      });
-
-    // show current held item
-    if (
-      currentGame
-        .getCurrentPlayer()
-        .getSelectedItem()
-        .isKeyItem() == true
-    ) {
-      // key items won't need to display a quantity
-      heldItemDisplay.innerHTML = currentGame
-        .getCurrentPlayer()
-        .getSelectedItem()
-        .getDisplayName();
-    } else {
-      heldItemDisplay.innerHTML =
+        // show buttons for each inventory item
+        inventoryButtonsDiv.innerHTML = ""; // wipe this clean!
         currentGame
-          .getCurrentPlayer()
-          .getSelectedItem()
-          .getDisplayName() +
-        " x " +
-        currentGame
-          .getCurrentPlayer()
-          .getSelectedItem()
-          .getQuantity();
-    }
-
-    // RENDER MAP
-
-    let currentMap = currentGame.getCurrentRoom().getTileMap();
-
-    for (let xStep = 0; xStep < currentMap.length; xStep++) {
-      for (let yStep = 0; yStep < currentMap[0].length; yStep++) {
-        let currentTile: PIXI.Sprite;
-
-        // set appropriate texture
-        switch (currentMap[xStep][yStep].getType()) {
-          default:
-          case "DIRT":
-            currentTile = new PIXI.Sprite(
-              app.loader.resources["assets/dirt.png"].texture
-            );
-            break;
-          case "WATER":
-            currentTile = new PIXI.Sprite(
-              app.loader.resources["assets/water.png"].texture
-            );
-            break;
-        }
-        currentTile.x = (xStep + 1) * 16 - 16;
-        currentTile.y = (yStep + 1) * 16 - 16;
-        currentTile.interactive = true; // tiles should be clickable!
-
-        let mouseDown = function() {
-          // use the player's held item to use this target
-          // if this was successful, result will be true
-          let result = currentGame
             .getCurrentPlayer()
-            .getSelectedItem()
-            .useItem(undefined, currentMap[xStep][yStep]);
+            .getInventory()
+            .getContents()
+            .forEach(function(item: item.Item) {
+                //sconsole.log("Creating button for item " + item.getDisplayName());
+                let itemButton = document.createElement("BUTTON");
+                if (item.isKeyItem() == true) {
+                    // key items won't need to display a quantity
+                    itemButton.innerHTML = item.getDisplayName();
+                } else {
+                    itemButton.innerHTML =
+                        item.getDisplayName() + " x " + item.getQuantity();
+                }
 
-          if (!result) {
-            displayToastNotification("That didn't work.");
-          } else {
-            render(); // update screen
-          }
-        };
+                itemButton.onclick = function() {
+                    currentGame.getCurrentPlayer().setSelectedItem(item.getItemType());
+                };
 
-        currentTile.on("mousedown", mouseDown);
-        app.stage.addChild(currentTile);
-      }
-    }
+                inventoryButtonsDiv.appendChild(itemButton);
+            });
 
-    // RENDER ENTITIES
-    let currentEntities = currentGame.getCurrentRoom().getEntities();
-
-    currentEntities.forEach(function(currentEntity: entity.Entity) {
-      let currentEntityDisplayName = currentEntity.getDisplayName();
-      let currentEntitySprite: PIXI.Sprite;
-      switch (currentEntity.getType()) {
-        case entity.ENTITY_TYPE.PLANT_LIFEBUD:
-          // TODO: different sprites for lifebud growth levels
-          currentEntitySprite = new PIXI.Sprite(
-            app.loader.resources["assets/lifebud.png"].texture
-          );
-          break;
-
-        case entity.ENTITY_TYPE.OBJECT_STONE:
-          currentEntitySprite = new PIXI.Sprite(
-            app.loader.resources["assets/stone.png"].texture
-          );
-          break;
-
-        default:
-          currentEntitySprite = new PIXI.Sprite(
-            app.loader.resources["assets/player.png"].texture
-          );
-          break;
-      }
-      currentEntitySprite.x = (currentEntity.getPosition().x + 1) * 16 - 16;
-      currentEntitySprite.y = (currentEntity.getPosition().y + 1) * 16 - 16;
-      currentEntitySprite.interactive = true; // tiles should be clickable!
-
-      let mouseDown = function() {
-        dismissTooltip(); // since mouseout doesn't get triggered when you click it
-        // use the player's held item to use this target
-        // if this was successful, result will be true
-        let result = currentGame
-          .getCurrentPlayer()
-          .getSelectedItem()
-          .useItem(currentEntity);
-
-        if (!result) {
-          displayToastNotification("That didn't work.");
+        // show current held item
+        if (
+            currentGame
+                .getCurrentPlayer()
+                .getSelectedItem()
+                .isKeyItem() == true
+        ) {
+            // key items won't need to display a quantity
+            heldItemDisplay.innerHTML = currentGame
+                .getCurrentPlayer()
+                .getSelectedItem()
+                .getDisplayName();
         } else {
-          render(); // update screen
+            heldItemDisplay.innerHTML =
+                currentGame
+                    .getCurrentPlayer()
+                    .getSelectedItem()
+                    .getDisplayName() +
+                " x " +
+                currentGame
+                    .getCurrentPlayer()
+                    .getSelectedItem()
+                    .getQuantity();
         }
-      };
 
-      currentEntitySprite.on("mousedown", mouseDown); // pass through to its click function
-      currentEntitySprite.on("mouseover", () => {
-        // what do we do when the mouse is over this?
-        displayTooltip(currentEntityDisplayName);
-      });
-      currentEntitySprite.on("mouseout", () => {
-        // what do we do when the mouse leaves?
-        dismissTooltip();
-      });
+        // RENDER MAP
 
-      app.stage.addChild(currentEntitySprite);
-    });
-  }
+        let currentMap = currentGame.getCurrentRoom().getTileMap();
+
+        for (let xStep = 0; xStep < currentMap.length; xStep++) {
+            for (let yStep = 0; yStep < currentMap[0].length; yStep++) {
+                let currentTile: PIXI.Sprite;
+
+                // set appropriate texture
+                switch (currentMap[xStep][yStep].getType()) {
+                    default:
+                    case TILE_TYPE.DIRT:
+                        currentTile = new PIXI.Sprite(
+                            app.loader.resources["assets/dirt.png"].texture
+                        );
+                        break;
+                    case TILE_TYPE.WATER:
+                        currentTile = new PIXI.Sprite(
+                            app.loader.resources["assets/water.png"].texture
+                        );
+                        break;
+                }
+                currentTile.x = (xStep + 1) * 16 - 16;
+                currentTile.y = (yStep + 1) * 16 - 16;
+                currentTile.interactive = true; // tiles should be clickable!
+
+                let mouseDown = function() {
+                    // use the player's held item to use this target
+                    // if this was successful, result will be true
+                    let result = currentGame
+                        .getCurrentPlayer()
+                        .getSelectedItem()
+                        .useItem(undefined, currentMap[xStep][yStep]);
+
+                    if (!result) {
+                        displayToastNotification("That didn't work.");
+                    } else {
+                        render(); // update screen
+                    }
+                };
+
+                currentTile.on("mousedown", mouseDown);
+                app.stage.addChild(currentTile);
+            }
+        }
+
+        // RENDER ENTITIES
+        let currentEntities = currentGame.getCurrentRoom().getEntities();
+
+        currentEntities.forEach(function(currentEntity: entity.Entity) {
+            let currentEntityDisplayName = currentEntity.getDisplayName();
+            let currentEntitySprite: PIXI.Sprite;
+            switch (currentEntity.getType()) {
+                case entity.ENTITY_TYPE.PLANT_LIFEBUD:
+                    // TODO: different sprites for lifebud growth levels
+                    currentEntitySprite = new PIXI.Sprite(
+                        app.loader.resources["assets/lifebud.png"].texture
+                    );
+                    break;
+
+                case entity.ENTITY_TYPE.OBJECT_STONE:
+                    currentEntitySprite = new PIXI.Sprite(
+                        app.loader.resources["assets/stone.png"].texture
+                    );
+                    break;
+
+                default:
+                    currentEntitySprite = new PIXI.Sprite(
+                        app.loader.resources["assets/player.png"].texture
+                    );
+                    break;
+            }
+            currentEntitySprite.x = (currentEntity.getPosition().x + 1) * 16 - 16;
+            currentEntitySprite.y = (currentEntity.getPosition().y + 1) * 16 - 16;
+            currentEntitySprite.interactive = true; // tiles should be clickable!
+
+            let mouseDown = function() {
+                dismissTooltip(); // since mouseout doesn't get triggered when you click it
+                // use the player's held item to use this target
+                // if this was successful, result will be true
+                let result = currentGame
+                    .getCurrentPlayer()
+                    .getSelectedItem()
+                    .useItem(currentEntity);
+
+                if (!result) {
+                    displayToastNotification("That didn't work.");
+                } else {
+                    render(); // update screen
+                }
+            };
+
+            currentEntitySprite.on("mousedown", mouseDown); // pass through to its click function
+            currentEntitySprite.on("mouseover", () => {
+                // what do we do when the mouse is over this?
+                displayTooltip(currentEntityDisplayName);
+            });
+            currentEntitySprite.on("mouseout", () => {
+                // what do we do when the mouse leaves?
+                dismissTooltip();
+            });
+
+            app.stage.addChild(currentEntitySprite);
+        });
+    }
 }
 
 // show a tooltip on the screen (only one at a time!)
 function displayTooltip(text: string) {
-  console.log("Mouse moved over " + text);
-  //TODO: display tooltip
+    console.log("Mouse moved over " + text);
+    //TODO: display tooltip
 }
 
 // dismiss the visible tooltip (only one at a time!)
 function dismissTooltip() {
-  console.log("Mouse left");
-  // TODO: dismiss tooltip
+    console.log("Mouse left");
+    // TODO: dismiss tooltip
 }
 
 // shows a toast notification (this can be called from other files!)
 // we assume 2000ms if a duration is not specified
 export function displayToastNotification(text: string, duration?: number) {
-  // TODO: make toast less temporary
-  temporaryToastDiv.innerHTML = text;
-  let actualDuration = 2000;
-  if (duration != undefined) actualDuration = duration;
+    // TODO: make toast less temporary
+    temporaryToastDiv.innerHTML = text;
+    let actualDuration = 2000;
+    if (duration != undefined) actualDuration = duration;
 
-  /*setTimeout(() => {
-    temporaryToastDiv.innerHTML = "";
-  }, actualDuration);*/ //TODO: fix timeout
+    /*setTimeout(() => {
+      temporaryToastDiv.innerHTML = "";
+    }, actualDuration);*/ //TODO: fix timeout
 }
