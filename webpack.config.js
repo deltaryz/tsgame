97fb1e421634956b83714050744369d59b42a7e2
techniponi <wincam97@gmail.com>
2019-11-14 00:24:06 -0600

    fix webpack to use proper path in index.html

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,7 @@ module.exports = {
   ],
   output: {
     path: path.resolve(__dirname, "dist"),
-    publicPath: "/dist/",
+    publicPath: "/",
     filename: "bundle.js",
     chunkFilename: "[name].js"
   },
